6ed885c72f8a0d34d72ae32351ab5fdff5a6d227
Saran <saranshajibabu@gmail.com>
2014-07-02 12:22:07 +0800

    Git test commit

--- src/com/Saran/ribbit/MainActivity.java
+++ src/com/Saran/ribbit/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);   
 		
 		//If the user is not logged in, route to login activity
 		ParseUser currentUser = ParseUser.getCurrentUser();
