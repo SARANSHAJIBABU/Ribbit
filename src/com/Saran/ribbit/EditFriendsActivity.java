95174127f026b2d6624099afbac8aa4ace16de32
Saran <saranshajibabu@gmail.com>
2014-07-03 14:51:28 +0800

    Added checkmark to friends in user listview in EditFriendsActivity

--- src/com/Saran/ribbit/EditFriendsActivity.java
+++ src/com/Saran/ribbit/EditFriendsActivity.java
@@ -1,37 +1,48 @@
 package com.Saran.ribbit;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.Saran.ribbit.constants.ParseConstants;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class EditFriendsActivity extends ListActivity {
 	
-	private String[] mUsers;
+	private List<ParseUser> mUsers;
+	private ParseRelation<ParseUser> mFriendsRelation;
+	private ParseUser mCurrentUser;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_edit_friends);
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 	}
 	
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+		mCurrentUser = ParseUser.getCurrentUser();
+		mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+		
 		setProgressBarIndeterminateVisibility(true);
 		
 		//Query to get all users from parse
@@ -52,14 +63,32 @@ public class EditFriendsActivity extends ListActivity {
 					AlertDialog dialog = builder.create();
 					dialog.show();
 				}else{
+					mUsers = list;
+					String[] users = new String[mUsers.size()];
 					int i=0;
-					mUsers = new String[list.size()];
+					
 					for(ParseUser user: list){
-						mUsers[i] = user.getUsername();
+						users[i] =  user.getUsername();
 						i++;
 					}
-					ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this, android.R.layout.simple_list_item_checked,mUsers);
+					ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this, android.R.layout.simple_list_item_checked,users);
 					setListAdapter(adapter);
+					
+					mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+
+						@Override
+						public void done(List<ParseUser> friends,ParseException e) {
+							for(int i =0; i<mUsers.size();i++){
+								ParseUser user = mUsers.get(i);
+								
+								for(ParseUser friend: friends){
+									if(friend.getObjectId().equals(user.getObjectId())){
+										getListView().setItemChecked(i, true);
+									}
+								}
+							}
+						}
+					});
 				}
 			}
 		});
@@ -83,5 +112,28 @@ public class EditFriendsActivity extends ListActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		
+		if(getListView().isItemChecked(position)){
+			mFriendsRelation.add(mUsers.get(position));
+			mCurrentUser.saveInBackground(new SaveCallback() {
+				
+				@Override
+				public void done(ParseException e) {
+					if(e!=null){
+						//error
+					}
+					
+				}
+			});
+		}else{
+			
+		}
+
 	}
 }
--- src/com/Saran/ribbit/constants/ParseConstants.java
+++ src/com/Saran/ribbit/constants/ParseConstants.java
@@ -3,5 +3,5 @@ package com.Saran.ribbit.constants;
 public final class ParseConstants {
 	
 	public static final String KEY_USERNAME = "username";
-
+	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
 }
